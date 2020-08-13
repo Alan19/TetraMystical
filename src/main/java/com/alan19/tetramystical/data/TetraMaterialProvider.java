22f71247e38d302b17b7adaff04fb0ee4ff5973f
Alan19 <Alan19@users.noreply.github.com>
2020-08-13 17:11:04 -0400

    Add other parts, add tool replacement jsons

--- src/main/java/com/alan19/tetramystical/data/TetraMaterialProvider.java
+++ src/main/java/com/alan19/tetramystical/data/TetraMaterialProvider.java
@@ -64,9 +64,12 @@ public class TetraMaterialProvider implements IDataProvider {
 
         // data shared between all module variants of the same materials are defined here, e.g. the localized
         ModuleBuilder.Material amethyst = new ModuleBuilder.Material("amethyst", "amethyst", 0xd9f3ccf, 0x9f3ccf,
-                3, 70, "tag", "forge:gems/amethyst", 1, Capability.hammer, 2, gemReferences, Pair.of("enchantment/looting", 1), Pair.of("enchantment/fortune", 1));
+                1, 70, "tag", "forge:gems/amethyst", 1, Capability.hammer, 2, gemReferences, Pair.of("enchantment/looting", 1), Pair.of("enchantment/fortune", 1));
         ModuleBuilder.Material tin = new ModuleBuilder.Material("tin", "tin", 0xd9f3ccf, 0xd9f3ccf, 0, 72, "tag", "forge:ingots/tin", 1, Capability.hammer, 1, metalReferences).addVariantData("reach", 0.25);
-        ModuleBuilder.Material lead = new ModuleBuilder.Material("lead", "lead", 0xd9f3ccf, 0xd9f3ccf, 0, 10, "tag", "forge:ingots/tin", 1, Capability.hammer, 2, metalReferences).addItemEffect(ItemEffect.toughness, 1);
+        ModuleBuilder.Material lead = new ModuleBuilder.Material("lead", "lead", 0xd9f3ccf, 0xd9f3ccf, 0, 66, "tag", "forge:ingots/lead", 1, Capability.hammer, 2, metalReferences).addItemEffect(ItemEffect.toughness, 1);
+        ModuleBuilder.Material quicksilver = new ModuleBuilder.Material("quicksilver", "quicksilver", 0xB4C3C4, 0xB4C3C4, 1, 110, "tag", "forge:ingots/quicksilver", 1, Capability.hammer, 1, metalReferences).addItemEffect(ItemEffect.quickStrike, 1);
+        ModuleBuilder.Material cactus = new ModuleBuilder.Material("cactus", "cactus", 0x649832, 0x649832, 0, 15, "item", "minecraft:cactus", 1, Capability.cut, 1, woodReferences);
+
 
         // Setup each module which should have additional variants generated here, using the materials defined above. The material is paired with an
         // an item below which is then used to grab additional data for the module, e.g. damage, mining speed or durability. There are several methods
@@ -79,43 +82,98 @@ public class TetraMaterialProvider implements IDataProvider {
                 "double/basic_pickaxe/basic_pickaxe") // the path for the schema file, I've not been consistent in how I've structured this so double check that this is correct
                 .offsetDurability(-20, 0.5f) // pickaxes have two heads and the default handle has 20 durability so the durability of the module should be = (itemDurability - 20) * 0.5
                 .offsetSpeed(0, 0.5f) // same math goes for the speed, the flimsy handle has no impact on speed so the speed of the item should be split equally between the heads
-                .addVariant(amethyst, "mysticalworld:amethyst_pickaxe");
+                .addVariant(amethyst, getItemName("pickaxe", amethyst))
+                .addVariant(quicksilver, getItemName("pickaxe", quicksilver))
+                .addVariant(cactus, getItemName("pickaxe", cactus));
 
         setupModule("double/basic_axe", "basic_axe", "%s axe", "basic_axe/iron", "double/basic_axe/basic_axe")
                 .offsetOutcome(2, 0) // offsets the amount of material required (defined per material above) by a multiplier of two
-                .offsetDurability(-20, 0.7f)
+                .offsetDurability(-20, 0.75f)
                 .offsetSpeed(-0.1f, 1)
-                .addVariant(amethyst, "mysticalworld:amethyst_axe");
+                .addVariant(amethyst, getItemName("axe", amethyst))
+                .addVariant(quicksilver, getItemName("axe", quicksilver))
+                .addVariant(cactus, getItemName("axe", cactus));
+
+        setupModule("double/hoe", "hoe", "%s hoe","hoe/iron", "double/hoe/hoe")
+                .offsetOutcome(2, 0)
+                .offsetDurability(-20, 0.75f)
+                .offsetSpeed(-0.1f, 1)
+                .addVariant(amethyst, getItemName("hoe", amethyst))
+                .addVariant(quicksilver, getItemName("hoe", quicksilver))
+                .addVariant(cactus, getItemName("hoe", cactus));
 
         setupModule("double/butt", "butt", "%s butt", "butt/iron", "double/butt/butt")
                 .offsetOutcome(1, -1)
-                .addVariant(amethyst);
+                .offsetDurability(0, .25f)
+                .addVariant(amethyst, getItemName("axe", amethyst))
+                .addVariant(quicksilver, getItemName("axe", quicksilver))
+                .addVariant(cactus, getItemName("axe", cactus));
 
         setupModule("sword/basic_blade", "basic_blade", "%s blade", "basic_blade/iron", "sword/basic_blade")
-                .offsetDurability(-10, 1)
-                .addVariant(amethyst, "mysticalworld:amethyst_sword");
+                .offsetSpeed(-.45f, 1)
+                .addVariant(amethyst, getItemName("sword", amethyst))
+                .addVariant(quicksilver, getItemName("sword", quicksilver))
+                .addVariant(cactus, getItemName("sword", cactus));
 
         setupModule("sword/heavy_blade", "heavy_blade", "Heavy %s blade", "heavy_blade/iron", "sword/heavy_blade")
                 .offsetIntegrity(-1)
                 .offsetSpeed(-.575f, .25f)
                 .offsetDurability(-10, 1)
                 .offsetOutcome(8, 0)
-                .addVariant(amethyst);
+                .addVariant(amethyst, getItemName("sword", amethyst))
+                .addVariant(quicksilver, getItemName("sword", quicksilver))
+                .addVariant(cactus, getItemName("sword", cactus));
 
         setupModule("sword/machete", "machete", "%s machete", "machete/iron", "sword/machete")
                 .offsetIntegrity(-1)
-                .offsetSpeed((float) (-.2 - 8 / 9f), 1.5f)
+                .offsetSpeed(-1.0888888f, 1.5f)
                 .offsetOutcome(2, 0)
-                .addVariant(amethyst);
+                .addVariant(amethyst, getItemName("sword", amethyst))
+                .addVariant(quicksilver, getItemName("sword", quicksilver))
+                .addVariant(cactus, getItemName("sword", cactus));
 
         setupModule("sword/short_blade", "short_blade", "%s short_blade", "short_blade/iron", "sword/short_blade")
-                .offsetSpeed(1 + 1 / 30f, 7 / 3f)
                 .offsetOutcome(1, 1)
-                .addVariant(amethyst, "mysticalworld:amethyst_knife");
+                .addVariant(amethyst, getItemName("knife", amethyst))
+                .addVariant(quicksilver, getItemName("knife", quicksilver))
+                .addVariant(cactus, getItemName("knife", cactus));
 
         setupModule("sword/socket", "sword_socket", "%s", "socket/diamond", "sword/socket")
                 .addVariant(tin)
-                .addVariant(lead);
+                .addVariant(lead)
+                .addVariant(quicksilver)
+                .addVariant(amethyst);
+
+        setupModule("double/socket", "double_socket", "%s", "socket/diamond", "double/socket")
+                .addVariant(tin)
+                .addVariant(lead)
+                .addVariant(quicksilver)
+                .addVariant(amethyst);
+
+        setupModule("single/socket", "single_socket", "%s", "socket/diamond", "single/socket")
+                .addVariant(tin)
+                .addVariant(lead)
+                .addVariant(quicksilver)
+                .addVariant(amethyst);
+
+
+        setupModule("single/basic_shovel", "basic_shovel", "%s shovel", "basic_shovel/iron", "single/basic_shovel/basic_shovel")
+                .offsetDurability(-20, 0.65f)
+                .addVariant(amethyst, getItemName("shovel", amethyst))
+                .addVariant(quicksilver, getItemName("shovel", quicksilver))
+                .addVariant(cactus, getItemName("shovel", cactus));
+
+        setupModule("single/spearhead", "spearhead", "%s spearhead", "spearhead/iron", "single/spearhead/spearhead")
+                .offsetDurability(-20, 1)
+                .offsetOutcome(2, 0)
+                .addVariant(amethyst, getItemName("spear", amethyst))
+                .addVariant(quicksilver, getItemName("spear", quicksilver))
+                .addVariant(cactus, getItemName("spear", cactus));
+
+    }
+
+    private String getItemName(String tool, ModuleBuilder.Material material) {
+        return String.format("mysticalworld:%s_%s", material.getKey(), tool);
     }
 
     @Override
